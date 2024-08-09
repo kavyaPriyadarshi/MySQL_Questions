494dace480bcdac48676ef10ef9c6e30c981c220
kavyaPriyadarshi <142102777+kavyaPriyadarshi@users.noreply.github.com>
2024-08-09 15:47:51 +0530

    Create 1164.Product_Price_at_Given_Date.sql

---
+++ 1164.Product_Price_at_Given_Date.sql
@@ -0,0 +1,69 @@
+/*
+Table: Products
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| new_price     | int     |
+| change_date   | date    |
++---------------+---------+
+(product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+Each row of this table indicates that the price of some product was changed to a new price at some date.
+ 
+
+Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output: 
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
+
+*/
+SELECT 
+    PRODUCT_ID,
+    IFNULL(
+        (
+            SELECT 
+                NEW_PRICE 
+            FROM 
+                PRODUCTS AS p2
+            WHERE 
+                p2.PRODUCT_ID = p1.PRODUCT_ID 
+                AND p2.CHANGE_DATE <= '2019-08-16'
+            ORDER BY 
+                p2.CHANGE_DATE DESC
+            LIMIT 1
+        ), 
+        10
+    ) AS price 
+FROM 
+    PRODUCTS AS p1
+GROUP BY 
+    PRODUCT_ID
+ORDER BY 
+    PRODUCT_ID DESC;
